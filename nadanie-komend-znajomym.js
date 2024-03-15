2df5c77ca01bf76aacd5e93cd075e92395b39a71
Kacper Gruszczyński <58849959+Dunkhell@users.noreply.github.com>
2024-03-15 14:05:47 +0100

    Update nadanie-komend-znajomym.js

--- nadanie-komend-znajomym.js
+++ nadanie-komend-znajomym.js
@@ -1,37 +1,47 @@
-const tableRows = document.querySelectorAll('form table.vis tbody tr');
-let index = 0;
-function processRow() {
-    const row = tableRows[index];
-    console.log(row);
+if (typeof zabierzKomendyPozostalym === 'undefined') {
+    alert('Skrypt został zaktualizowany, używasz starej wersji. Sprawdź najnowszą na forum plemiona w skryptotece!')
     
-    if (row) {
-        index++;
-        if (index === 1 || index === tableRows.length - 1) {
-            setTimeout(processRow, 10);
-        }
-        const playerName = row.querySelector('td a').innerText.trim();
-        console.log(playerName);
-
-        if (trusted.includes(playerName)) {
-        console.log("pressing for " + playerName); 
-        const shareCheckbox = row.querySelector('td input[name="share[]"]');
-            const viewCheckbox = row.querySelector('td input[name="view[]"]');
-            
-            if (shareCheckbox && viewCheckbox) {
-                shareCheckbox.checked = true;
-                viewCheckbox.checked = true;
+} else {
+    const tableRows = document.querySelectorAll('form table.vis tbody tr');
+    let index = 0;
+    function processRow() {
+        const row = tableRows[index];
+        console.log(row);
+        
+        if (row) {
+            index++;
+            if (index === 1 || index === tableRows.length - 1) {
+                setTimeout(processRow, 10);
             }
-            setTimeout(processRow, 100); 
+            const playerName = row.querySelector('td a').innerText.trim();
+            console.log(playerName);
+    
+            if (trusted.includes(playerName)) {
+            console.log("pressing for " + playerName); 
+            const shareCheckbox = row.querySelector('td input[name="share[]"]');
+                const viewCheckbox = row.querySelector('td input[name="view[]"]');
+                
+                if (shareCheckbox && viewCheckbox) {
+                    shareCheckbox.checked = true;
+                    viewCheckbox.checked = true;
+                } else {
+                    if (zabierzKomendyPozostalym) {
+                        shareCheckbox.checked = false;
+                    }                
+                }
+                setTimeout(processRow, 100); 
+            }
+            setTimeout(processRow, 10); 
+        } else {
+            setTimeout(clickSaveButton, 50); 
         }
-        setTimeout(processRow, 10); 
-    } else {
-        setTimeout(clickSaveButton, 50); 
+    }
+    processRow();
+    function clickSaveButton() {
+        const saveButton = document.querySelector('input[type="submit"].btn');
+        if (saveButton) {
+            saveButton.click();
+        }
     }
 }
-processRow();
-function clickSaveButton() {
-    const saveButton = document.querySelector('input[type="submit"].btn');
-    if (saveButton) {
-        saveButton.click();
-    }
-}
+
