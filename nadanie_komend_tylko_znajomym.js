68ebee6ba907d2a4270f8cadcb9798d8c721ced6
Kacper Gruszczyński <58849959+Dunkhell@users.noreply.github.com>
2024-05-22 21:05:10 +0200

    Fixed new config

--- nadanie_komend_tylko_znajomym.js
+++ nadanie_komend_tylko_znajomym.js
@@ -1,5 +1,9 @@
-var trusted = config.trusted;
+
+var trusted = config.trusted.map(item => item.trim().toLowerCase());
 var zabierzKomendyPozostalym = config.zabierzKomendyPozostalym;
+
+console.log(Array.isArray(trusted));
+console.log(typeof zabierzKomendyPozostalym);
 
 if (typeof zabierzKomendyPozostalym === 'undefined') {
     alert('Skrypt zostal zaktualizowany, uzywasz starej wersji. sprawdz najnowsza na forum plemiona w skryptotece!')
@@ -16,10 +20,11 @@ if (typeof zabierzKomendyPozostalym === 'undefined') {
             if (index === 1 || index === tableRows.length - 1) {
                 setTimeout(processRow, 10);
             }
-            const playerName = row.querySelector('td a').innerText.trim();
+            const playerName = row.querySelector('td a').innerText.trim().toLowerCase();
             const shareCheckbox = row.querySelector('td input[name="share[]"]');
             const viewCheckbox = row.querySelector('td input[name="view[]"]');
             if (trusted.includes(playerName)) {
+				console.log(playerName);
                 if (shareCheckbox && viewCheckbox) {
                     shareCheckbox.checked = true;
                     viewCheckbox.checked = true;
