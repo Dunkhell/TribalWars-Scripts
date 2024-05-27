c0f7e4bc3646fc2540c11049e127de0b7903ea9a
Kacper Gruszczyński <58849959+Dunkhell@users.noreply.github.com>
2024-05-28 00:09:29 +0200

    Update nadanie_komend_tylko_znajomym.js

--- nadanie_komend_tylko_znajomym.js
+++ nadanie_komend_tylko_znajomym.js
@@ -1,45 +1,45 @@
-
-var trusted = config.trusted.map(item => item.trim().toLowerCase());
-var zabierzKomendyPozostalym = config.zabierzKomendyPozostalym;
-
-if (typeof zabierzKomendyPozostalym === 'undefined') {
-    alert('Skrypt zostal zaktualizowany, uzywasz starej wersji. sprawdz najnowsza na forum plemiona w skryptotece!')
-    
+if (typeof config !== 'undefined') {
+    var zabierzKomendyPozostalym = config.zabierzKomendyPozostalym;
+    var trusted = config.trusted.map(item => item.trim().toLowerCase());;
 } else {
-    const tableRows = document.querySelectorAll('form table.vis tbody tr');
-    let index = 0;
-    function processRow() {
-        const row = tableRows[index];
-        
-        if (row) {
-            index++;
-            if (index === 1 || index === tableRows.length - 1) {
-                setTimeout(processRow, 10);
-            }
-            const playerName = row.querySelector('td a').innerText.trim().toLowerCase();
-            const shareCheckbox = row.querySelector('td input[name="share[]"]');
-            const viewCheckbox = row.querySelector('td input[name="view[]"]');
-            if (trusted.includes(playerName)) {
-                if (shareCheckbox && viewCheckbox) {
-                    shareCheckbox.checked = true;
-                    viewCheckbox.checked = true;
-                }
-                setTimeout(processRow, 100); 
-            }  else {
-                if (zabierzKomendyPozostalym) {
-                    shareCheckbox.checked = false;
-                }                
-            }
-            setTimeout(processRow, 10); 
-        } else {
-            setTimeout(clickSaveButton, 50); 
+    var zabierzKomendyPozostalym = typeof zabierzKomendyPozostalym !== 'undefined' ? zabierzKomendyPozostalym : true;
+    var trusted = typeof trusted !== 'undefined' ? trusted.map(item => item.trim().toLowerCase()) : [];
+}
+
+const tableRows = document.querySelectorAll('form table.vis tbody tr');
+let index = 0;
+function processRow() {
+    const row = tableRows[index];
+    
+    if (row) {
+        index++;
+        if (index === 1 || index === tableRows.length - 1) {
+            setTimeout(processRow, 10);
         }
-    }
-    processRow();
-    function clickSaveButton() {
-        const saveButton = document.querySelector('input[type="submit"].btn');
-        if (saveButton) {
-            saveButton.click();
+        const playerName = row.querySelector('td a').innerText.trim().toLowerCase();
+        const shareCheckbox = row.querySelector('td input[name="share[]"]');
+        const viewCheckbox = row.querySelector('td input[name="view[]"]');
+        if (trusted.includes(playerName)) {
+            if (shareCheckbox && viewCheckbox) {
+                shareCheckbox.checked = true;
+                viewCheckbox.checked = true;
+            }
+            setTimeout(processRow, 100); 
+        }  else {
+            if (zabierzKomendyPozostalym) {
+                shareCheckbox.checked = false;
+            }                
         }
+        setTimeout(processRow, 10); 
+    } else {
+        setTimeout(clickSaveButton, 50); 
     }
 }
+processRow();
+function clickSaveButton() {
+    const saveButton = document.querySelector('input[type="submit"].btn');
+    if (saveButton) {
+        saveButton.click();
+    }
+}
+
